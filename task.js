3b3b21a1518a3301bbad5268f5f6ad364399ba62
yuekai.wyk <yuekai.wyk@alibaba-inc.com>
2024-11-30 10:20:44 +0800

    update

--- task.js
+++ task.js
@@ -1,9 +1,25 @@
 'use strict'
 
 var fs = require('fs-extra');
+const axios  = require('axios');
+const moment = require('moment');
 
 async function start() {
-  await fs.writeFile('README.md', new Date().getTime() + '');
-  console.log('更新README.md成功');
+
+  const response = await axios.get('https://danjuanfunds.com/djapi/index_eva/detail/NDX');
+
+  const data = response?.data?.data || {};
+
+  if (data?.updated_at) {
+    const date = new moment(data.updated_at).format('YYYY-MM-DD')
+
+    console.info(date);
+
+    fs.outputFileSync(`${__dirname}/ndx/${date}.json`, JSON.stringify(data, null, '  '))
+
+    console.log(`更新 ${date} 成功`);
+  } else {
+    throw Error(`更新失败，数据为空`)
+  }
 }
 start();
